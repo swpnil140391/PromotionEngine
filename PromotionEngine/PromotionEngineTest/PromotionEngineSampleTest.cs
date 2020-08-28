ac5d8f3ae17cac6ac2bb9c82703f70ac92ce0329
swpnil.vyas@softdel.com <swpnil.vyas@softdel.com>
2020-08-29 01:59:02 +0530

    Scenario -3 Unit test case added

--- PromotionEngine/PromotionEngineTest/PromotionEngineSampleTest.cs
+++ PromotionEngine/PromotionEngineTest/PromotionEngineSampleTest.cs
@@ -50,6 +50,26 @@ namespace PromotionEngineTest
             Assert.AreEqual(OperationManager._BillingManager.PerformOperation(2), 280);
         }
 
+        [TestMethod]
+        public void TestMethod3()
+        {
+            OperationManager._PurchaseManager.CurrentOrderDetails.Clear();
+            OrderDetails o1 = new OrderDetails();
+            o1.SKUID = "A";
+            o1.Quantity = 5;
+            OperationManager._PurchaseManager.CurrentOrderDetails.Add(o1);
+            o1 = new OrderDetails();
+            o1.SKUID = "B";
+            o1.Quantity = 5;
+            OperationManager._PurchaseManager.CurrentOrderDetails.Add(o1);
+            o1 = new OrderDetails();
+            o1.SKUID = "C";
+            o1.Quantity = 1;
+            OperationManager._PurchaseManager.CurrentOrderDetails.Add(o1);
+            
+            Assert.AreEqual(OperationManager._BillingManager.PerformOperation(2), 370);
+        }
+
 
     }
 }
