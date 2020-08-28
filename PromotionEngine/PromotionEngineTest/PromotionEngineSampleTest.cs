d3e6e9ef0b5e20ff34a3ca118212fc08c48dc52a
swpnil.vyas@softdel.com <swpnil.vyas@softdel.com>
2020-08-29 01:57:05 +0530

    Scenario 2 Test case added & unit testing Done

--- PromotionEngine/PromotionEngineTest/PromotionEngineSampleTest.cs
+++ PromotionEngine/PromotionEngineTest/PromotionEngineSampleTest.cs
@@ -11,6 +11,7 @@ namespace PromotionEngineTest
         [TestMethod]
         public void TestMethod1()
         {
+            OperationManager._PurchaseManager.CurrentOrderDetails.Clear();
             OrderDetails o1 = new OrderDetails();
             o1.SKUID = "A";
             o1.Quantity = 1;
@@ -25,5 +26,30 @@ namespace PromotionEngineTest
             OperationManager._PurchaseManager.CurrentOrderDetails.Add(o1);
             Assert.AreEqual(OperationManager._BillingManager.PerformOperation(2), 100);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            OperationManager._PurchaseManager.CurrentOrderDetails.Clear();
+            OrderDetails o1 = new OrderDetails();
+            o1.SKUID = "A";
+            o1.Quantity = 3;
+            OperationManager._PurchaseManager.CurrentOrderDetails.Add(o1);
+            o1 = new OrderDetails();
+            o1.SKUID = "B";
+            o1.Quantity = 5;
+            OperationManager._PurchaseManager.CurrentOrderDetails.Add(o1);
+            o1 = new OrderDetails();
+            o1.SKUID = "C";
+            o1.Quantity = 1;
+            OperationManager._PurchaseManager.CurrentOrderDetails.Add(o1);
+            o1 = new OrderDetails();
+            o1.SKUID = "D";
+            o1.Quantity = 1;
+            OperationManager._PurchaseManager.CurrentOrderDetails.Add(o1);
+            Assert.AreEqual(OperationManager._BillingManager.PerformOperation(2), 280);
+        }
+
+
     }
 }
